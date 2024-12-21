bd2fcfabf5133a847d042e9bb7a929ee8cfbf516
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2024-12-21 11:06:13 +0100

    adding imports

--- skl.py
+++ skl.py
@@ -1,2 +1,7 @@
-from sklearn import datasets
+
 import numpy as np
+from sklearn import datasets
+iris = datasets.load_iris()
+X = iris.data[:, [2,3]]
+y = iris.target
+print('Class labels :',np.unique(y))
