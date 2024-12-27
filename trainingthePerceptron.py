a76dd7651637101aa4bdd93f0b2621d3b3e5f652
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2024-12-27 10:54:39 +0100

    likelihood function

--- trainingthePerceptron.py
+++ trainingthePerceptron.py
@@ -129,3 +129,24 @@ ax = plt.gca()
 ax.yaxis.grid(True)
 plt.tight_layout()
 plt.show()
+
+def loss_1(z):
+    return - np.log(sigmoid(z))
+
+def loss_0(z):
+    return - np.log(1-sigmoid(z))
+
+z = np.arrange(-10, 10, 0.1)
+sigma_z = sigmoid(z)
+
+c1 = [loss_1(x) for x in z]
+plt.plot(sigma_z, c1, label='L(w, b) if y = 1')
+c0 = [loss_0(x) for x in z]
+plt.plot(sigma_z, c0, linestyle = '--', label = 'L(w,b) if y = 0')
+plt.ylim('.', 5.1)
+plt.xlim([0, 1])
+plt.xlabel('$\sigma(z)$')
+plt.ylabel('L(w, b)')
+plt.legend(loc='best')
+plt.tight_layout()
+plt.show()
